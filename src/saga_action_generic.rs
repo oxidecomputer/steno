105b6ea09adde1745035804a44dc037f360d2569
David Pacheco <dap@oxidecomputer.com>
2023-05-24 19:30:59 -0700

    error message edit

--- src/saga_action_generic.rs
+++ src/saga_action_generic.rs
@@ -167,7 +167,7 @@ impl<UserType: SagaType> Action<UserType> for ActionInjectError {
     fn undo_it(&self, _: ActionContext<UserType>) -> BoxFuture<'_, UndoResult> {
         // We should never undo an action that failed.  But this same impl is
         // plugged into a saga when an "undo action" error is injected.
-        Box::pin(futures::future::err(anyhow::anyhow!("injected error")))
+        Box::pin(futures::future::err(anyhow::anyhow!("error injected")))
     }
 
     fn name(&self) -> ActionName {
